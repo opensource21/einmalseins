f25e762640df759d35eb08be3e8334a29fc4d1ed
niels <opensource21@gmail.com>
2016-11-01 10:21:02 +0100

    Etwas aufgeräumt

--- src/components/FactorConfig.js
+++ src/components/FactorConfig.js
@@ -13,10 +13,7 @@ export default function FactorConfig({factorName, range, rangeChangeFunc}) {
             size = "2" min = "2" max = "30"
             onChange={event => 
                 rangeChangeFunc(factorName, event.target.name, event.target.value)}/>.
-    </div>
-    
-
-    )
+    </div>)
 }
 
 FactorConfig.propTypes = {
